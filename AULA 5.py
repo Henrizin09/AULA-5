99f25fdeeff2deac5a2f725a25f09b7f442a1750
Henrizin09 <josehenriquemota07@gmail.com>
2024-08-09 15:13:07 -0300

    Add files via upload

---
+++ AULA 5.py
@@ -0,0 +1,25 @@
+
+for numero in range(2, 21, 2):
+    print(numero)
+
+
+soma = 0
+for numero in range(5, 51, 5):
+    soma += numero
+    print(numero)
+
+print("Soma dos múltiplos de 5 até 50:", soma)
+
+
+
+
+variavel = 10
+print("O tipo da variável é:", type(variavel))
+
+
+
+nome = "Henrique"
+print(f"Olá, {nome}! Bem-vindo ao programa.")
+
+
+
